7d3ac41bf371e11cb7022f1245bce91996b11817
Kevin Diego <55944691+KevinDik@users.noreply.github.com>
2023-02-27 19:21:24 +0000

    style: Corrigindo o footer e adicionando o copy

---
+++ digital_burger/assets/js/header.js
@@ -0,0 +1,8 @@
+/** Gera o html do footer automagicamente precisando somente adicionar o link do script no html via head
+ * 
+ */
+
+const TAG = document.querySelector(".header");
+const HTML = (``)
+
+TAG.innerHTML = HTML;
