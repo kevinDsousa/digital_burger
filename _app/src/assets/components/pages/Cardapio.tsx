8fddb960d19545017600e4747758dc6ca106643d
kevin <kevindiegodasilvasousa@gmail.com>
2023-03-17 22:23:14 -0300

    feat: Criado componente base do footer

--- _app/src/assets/components/pages/Cardapio.tsx
+++ _app/src/assets/components/pages/Cardapio.tsx
@@ -1,7 +1,11 @@
+import Footer from "../layout/Footer";
 import Header from "../layout/Header";
 
 export default function Cardapio() {
     return(
-        <Header/>
+        <>
+            <Header/>
+            <Footer/>
+        </>
     )
 }
