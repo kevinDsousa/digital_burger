8892e7d79fb4f8b5df0204d8da22dfe2f914c7e9
kevin <kevindiegodasilvasousa@gmail.com>
2023-03-19 10:44:14 -0300

    feat: Criado componente do banner

--- _app/src/assets/components/pages/Cardapio.tsx
+++ _app/src/assets/components/pages/Cardapio.tsx
@@ -1,10 +1,13 @@
 import Footer from "../layout/Footer";
 import Header from "../layout/Header";
+import Banner from "../layout/Banner";
 
 export default function Cardapio() {
+    
     return(
         <>
             <Header/>
+            <Banner/>
             <Footer/>
         </>
     )
