1e15a74f04b59175a7567ec1180b846069485fda
kevin <kevindiegodasilvasousa@gmail.com>
2023-03-19 15:21:09 -0300

    wip: trabalhando em cima dos cards e objetos responsaveis por chamar os produtos

--- _app/src/assets/components/layout/Products.tsx
+++ _app/src/assets/components/layout/Products.tsx
@@ -4,21 +4,41 @@ import './Products.css'
 export default function Products() {
 
     const produtos = {
-        "nome": "PHP Burger", 
-        "url": "../../imagens/Produtos/Burgers/PhpBurger.jpg", 
-        "preco": 20.99, 
-        "descricao": "Pão de beterraba, burger suíno de 150g, bacon, queijo muçarela, maçã caramelizada, alface e maionese especial.", 
-        "tolerancia": ["../../imagens/Selos/glutem.jpg", "./../imagens/Selos/lactose.jpg", "./../imagens/Selos/sugar.jpg"]
+        burgers: {
+            "nome": "PHP Burger",
+            "url": "../../imagens/Produtos/Burgers/PhpBurger.jpg",
+            "preco": 20.99,
+            "descricao": "Pão de beterraba, burger suíno de 150g, bacon, queijo muçarela, maçã caramelizada, alface e maionese especial.",
+            "tolerancia": ["../../imagens/Selos/glutem.jpg", "./../imagens/Selos/lactose.jpg", "./../imagens/Selos/sugar.jpg"]
+        },
+        combos: {
+            "nome": "Combo Desenvolvedor Junior",
+            "url": "../../imagens/Produtos/Combo 1.jpg",
+            "preco": 35.99,
+            "descricao": "Pão de beterraba, burger suíno de 150g, bacon, queijo muçarela, maçã caramelizada, alface e maionese especial. Acompanha anéis de cebola e uma bebida a sua escolha.",
+            "tolerancia": ["../../imagens/Selos/glutem.jpg", "./../imagens/Selos/lactose.jpg", "./../imagens/Selos/sugar.jpg"]
+        }
     }
 
     return (
-        <div className='db-products'>
-            <Product nome={produtos.nome} url={produtos.url} preco={produtos.preco} descricao={produtos.descricao} tolerancia={produtos.tolerancia}/>
-            <Product nome={produtos.nome} url={produtos.url} preco={produtos.preco} descricao={produtos.descricao} tolerancia={produtos.tolerancia}/>
-            <Product nome={produtos.nome} url={produtos.url} preco={produtos.preco} descricao={produtos.descricao} tolerancia={produtos.tolerancia}/>
-            <Product nome={produtos.nome} url={produtos.url} preco={produtos.preco} descricao={produtos.descricao} tolerancia={produtos.tolerancia}/>
-            <Product nome={produtos.nome} url={produtos.url} preco={produtos.preco} descricao={produtos.descricao} tolerancia={produtos.tolerancia}/>
-            <Product nome={produtos.nome} url={produtos.url} preco={produtos.preco} descricao={produtos.descricao} tolerancia={produtos.tolerancia}/>
-        </div>
+        <>
+            <h2 className='db-products-title'>Combos</h2>
+            <div className='db-products'>
+                <Product nome={produtos.combos.nome} url={produtos.combos.url} preco={produtos.combos.preco} descricao={produtos.combos.descricao} tolerancia={produtos.combos.tolerancia}/>
+                <Product nome={produtos.combos.nome} url={produtos.combos.url} preco={produtos.combos.preco} descricao={produtos.combos.descricao} tolerancia={produtos.combos.tolerancia}/>
+                <Product nome={produtos.combos.nome} url={produtos.combos.url} preco={produtos.combos.preco} descricao={produtos.combos.descricao} tolerancia={produtos.combos.tolerancia}/>
+                <Product nome={produtos.combos.nome} url={produtos.combos.url} preco={produtos.combos.preco} descricao={produtos.combos.descricao} tolerancia={produtos.combos.tolerancia}/>
+            </div>
+
+            <h2 className='db-products-title'>Burgers</h2>
+            <div className='db-products'>
+                <Product nome={produtos.burgers.nome} url={produtos.burgers.url} preco={produtos.burgers.preco} descricao={produtos.burgers.descricao} tolerancia={produtos.burgers.tolerancia} />
+                <Product nome={produtos.burgers.nome} url={produtos.burgers.url} preco={produtos.burgers.preco} descricao={produtos.burgers.descricao} tolerancia={produtos.burgers.tolerancia} />
+                <Product nome={produtos.burgers.nome} url={produtos.burgers.url} preco={produtos.burgers.preco} descricao={produtos.burgers.descricao} tolerancia={produtos.burgers.tolerancia} />
+                <Product nome={produtos.burgers.nome} url={produtos.burgers.url} preco={produtos.burgers.preco} descricao={produtos.burgers.descricao} tolerancia={produtos.burgers.tolerancia} />
+                <Product nome={produtos.burgers.nome} url={produtos.burgers.url} preco={produtos.burgers.preco} descricao={produtos.burgers.descricao} tolerancia={produtos.burgers.tolerancia} />
+                <Product nome={produtos.burgers.nome} url={produtos.burgers.url} preco={produtos.burgers.preco} descricao={produtos.burgers.descricao} tolerancia={produtos.burgers.tolerancia} />
+            </div>
+        </>
     )
 }
