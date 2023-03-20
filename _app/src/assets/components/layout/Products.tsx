baf3612767a3ac91a335ac0e32a680c2bbbc134f
kevin <kevindiegodasilvasousa@gmail.com>
2023-03-20 11:47:16 -0300

    wip: Criado funcionalidade para percorrer a lista de burgers

--- _app/src/assets/components/layout/Products.tsx
+++ _app/src/assets/components/layout/Products.tsx
@@ -1,43 +1,58 @@
 import Product from './Product';
 import './Products.css'
 
+//Burgers
+import phpBurger from '../../../images/Produtos/Burgers/PhpBurger.jpg'
+import CplusBurger from '../../../images/Produtos/Burgers/CplusBurger.jpg'
+import GitMac from '../../../images/Produtos/Burgers/GitMac.jpg'
+import JavaBurger from '../../../images/Produtos/Burgers/JavaBurger.jpg'
+import PhpBurger from '../../../images/Produtos/Burgers/PhpBurger.jpg'
+import PythonBurger from '../../../images/Produtos/Burgers/PythonBurger.jpg'
+import ReactBurger from '../../../images/Produtos/Burgers/ReactBurger.jpg'
+
 export default function Products() {
 
-    const produtos = {
-        burgers: {
+    const burguers = [
+        {
             "nome": "PHP Burger",
-            "url": "../../imagens/Produtos/Burgers/PhpBurger.jpg",
+            "url": phpBurger,
             "preco": 20.99,
             "descricao": "Pão de beterraba, burger suíno de 150g, bacon, queijo muçarela, maçã caramelizada, alface e maionese especial.",
-            "tolerancia": ["../../imagens/Selos/glutem.jpg", "./../imagens/Selos/lactose.jpg", "./../imagens/Selos/sugar.jpg"]
+            "tolerancia": ["", "", ""],
         },
-        combos: {
-            "nome": "Combo Desenvolvedor Junior",
-            "url": "../../imagens/Produtos/Combo 1.jpg",
-            "preco": 35.99,
-            "descricao": "Pão de beterraba, burger suíno de 150g, bacon, queijo muçarela, maçã caramelizada, alface e maionese especial. Acompanha anéis de cebola e uma bebida a sua escolha.",
-            "tolerancia": ["../../imagens/Selos/glutem.jpg", "./../imagens/Selos/lactose.jpg", "./../imagens/Selos/sugar.jpg"]
-        }
-    }
+        {
+            "nome": "CplusBurger",
+            "url": CplusBurger,
+            "preco": 24.99,
+            "descricao": "Pão brioche, duas carnes de 100g, duas fatias de queijo cheddar, quatro anéis de cebola e nosso maravilhoso molho da casa.",
+            "tolerancia": ["", "", ""]
+        },
+        {
+            "nome": "GitMac",
+            "url": phpBurger,
+            "preco": 20.99,
+            "descricao": "Pão de beterraba, burger suíno de 150g, bacon, queijo muçarela, maçã caramelizada, alface e maionese especial.",
+            "tolerancia": ["", "", ""]
+        },
+        {
+            "nome": "Java Burger",
+            "url": JavaBurger,
+            "preco": 32.99,
+            "descricao": "São 3 hambúrgueres de Angus 120 g, 3 fatias de cheddar, 3 fatias de bacon, maionese temperada da casa, alface e tomate no pão brioche.",
+            "tolerancia": ["", "", ""]
+        },
+    ]
 
     return (
         <>
             <h2 className='db-products-title'>Combos</h2>
             <div className='db-products'>
-                <Product nome={produtos.combos.nome} url={produtos.combos.url} preco={produtos.combos.preco} descricao={produtos.combos.descricao} tolerancia={produtos.combos.tolerancia}/>
-                <Product nome={produtos.combos.nome} url={produtos.combos.url} preco={produtos.combos.preco} descricao={produtos.combos.descricao} tolerancia={produtos.combos.tolerancia}/>
-                <Product nome={produtos.combos.nome} url={produtos.combos.url} preco={produtos.combos.preco} descricao={produtos.combos.descricao} tolerancia={produtos.combos.tolerancia}/>
-                <Product nome={produtos.combos.nome} url={produtos.combos.url} preco={produtos.combos.preco} descricao={produtos.combos.descricao} tolerancia={produtos.combos.tolerancia}/>
+                {}
             </div>
 
             <h2 className='db-products-title'>Burgers</h2>
             <div className='db-products'>
-                <Product nome={produtos.burgers.nome} url={produtos.burgers.url} preco={produtos.burgers.preco} descricao={produtos.burgers.descricao} tolerancia={produtos.burgers.tolerancia} />
-                <Product nome={produtos.burgers.nome} url={produtos.burgers.url} preco={produtos.burgers.preco} descricao={produtos.burgers.descricao} tolerancia={produtos.burgers.tolerancia} />
-                <Product nome={produtos.burgers.nome} url={produtos.burgers.url} preco={produtos.burgers.preco} descricao={produtos.burgers.descricao} tolerancia={produtos.burgers.tolerancia} />
-                <Product nome={produtos.burgers.nome} url={produtos.burgers.url} preco={produtos.burgers.preco} descricao={produtos.burgers.descricao} tolerancia={produtos.burgers.tolerancia} />
-                <Product nome={produtos.burgers.nome} url={produtos.burgers.url} preco={produtos.burgers.preco} descricao={produtos.burgers.descricao} tolerancia={produtos.burgers.tolerancia} />
-                <Product nome={produtos.burgers.nome} url={produtos.burgers.url} preco={produtos.burgers.preco} descricao={produtos.burgers.descricao} tolerancia={produtos.burgers.tolerancia} />
+                {burguers.map(() => <Product nome={GitMac} url={GitMac} descricao={GitMac} preco={GitMac} tolerancia={[]}/>)}
             </div>
         </>
     )
